5433c007357e9034fa3c92c1d5b2c947e2ddcf5f
nitta <yosshi3800@gmail.com>
2021-07-26 14:12:18 +0900

    ゲームのステートを作りました。

---
+++ Assets/GameStates/PauseState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class GameManager
+{
+    /// <summary>
+    /// 一時停止状態
+    /// </summary>
+    public class PauseState : GameManagerBaseState
+    {
+        public override void OnEnter(GameManager owner, GameManagerBaseState prevbaseState)
+        {
+
+        }
+
+        public override void OnUpdate(GameManager owner)
+        {
+
+        }
+
+        public override void OnExit(GameManager owner, GameManagerBaseState nextState)
+        {
+
+        }
+    }
+}
